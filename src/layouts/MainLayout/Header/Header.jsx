5712964930bfc45cf094323b8a539d0f070dd7c5
lamhoang1256 <lamhoang1256@gmail.com>
2022-03-28 15:24:27 +0700

    finish responsive toggle menu

--- src/layouts/MainLayout/Header/Header.jsx
+++ src/layouts/MainLayout/Header/Header.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Carousel } from "./Carousel/Carousel";
 import "./header.scss";
 
 export const Header = () => {
@@ -11,17 +10,35 @@ export const Header = () => {
     { display: "Ứng dụng", path: "/" },
   ];
 
+  // change background Navbar from Transparent to White when scroll
+  const [isWhiteNav, setIsWhiteNav] = useState(false);
+  // lắng nghe sự kiện scroll trang
+  const listenScrollEvent = () => {
+    window.scrollY > 10 ? setIsWhiteNav(true) : setIsWhiteNav(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, []);
+
+  // xử lí toggle menu
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  const handleToggleMenu = () => {
+    setIsShowMenu(!isShowMenu);
+  };
+
   return (
-    <header className='header'>
-      {/* Hero Slider */}
-      <Carousel></Carousel>
+    <header className={`header ${isWhiteNav ? "header--white" : ""}`}>
       <div className='header__container'>
         <div className='container'>
           <div className='header__top'>
             <div className='header__logo'>
               <img src='./assets/logo-star-cineplex.png' alt='' />
             </div>
-            <div className='header__action'>
+            <div className={`header__action ${isShowMenu ? "is-show" : ""}`}>
+              {/* navbar link */}
               <ul className='navbar'>
                 {headerNav.map((item, index) => (
                   <li className='navbar__item' key={index}>
@@ -31,14 +48,37 @@ export const Header = () => {
                   </li>
                 ))}
               </ul>
+              {/* navbar register, login */}
               <div className='header__auth'>
                 <button className='header__register btn btn__secondary'>Đăng ký</button>
                 <button className='header__login btn btn__primary'>Đăng nhập</button>
               </div>
+              {/* navbar mobile close menu */}
+              <div className='navbar__close' onClick={handleToggleMenu}>
+                <ion-icon name='close-outline'></ion-icon>
+              </div>
             </div>
+            {/* navbar mobile open menu */}
+            <div className='navbar__open' onClick={handleToggleMenu}>
+              <ion-icon name='list-outline'></ion-icon>
+            </div>
+            {/* overplay */}
           </div>
         </div>
       </div>
+      {isShowMenu && <div className='header__overplay' onClick={handleToggleMenu}></div>}
     </header>
   );
 };
+
+// const toggle = document.querySelector(".menu-toggle");
+// const menu = document.querySelector(".menu");
+// const activeClass = "is-show";
+// toggle.addEventListener("click", function () {
+//   menu.classList.add(activeClass);
+// });
+// window.addEventListener("click", function (e) {
+//   if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+//     menu.classList.remove(activeClass);
+//   }
+// });
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Carousel } from "./components/Carousel/Carousel";
+import "./homePage.scss";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className='homePage'>
+      <Carousel />
+    </div>
+  );
 };
