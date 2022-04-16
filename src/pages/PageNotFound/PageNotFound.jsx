de5b6bc91c11aeae4ea145edbc5713159c2345a2
lamhoang1256 <lamhoang1256@gmail.com>
2022-04-16 11:01:52 +0700

    feat(404 page): basic UI 404 page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import "./pageNotFound.scss";
 
 export const PageNotFound = () => {
-  return <div>Not Found 404</div>;
+  return (
+    <div className='notfound'>
+      <div className='notfound-container'>
+        <div className='notfound-gradient'>404</div>
+        <h2 className='notfound-heading'>Không tìm thấy nội dung</h2>
+        <div className='notfound-desc'>
+          <p>URL của nội dung này đã bị thay đổi hoặc không còn tồn tại.</p>
+          <p>
+            Nếu bạn đang lưu URL này , hãy thử truy cập lại từ trang chủ thay vì dùng URL đã lưu.
+          </p>
+        </div>
+        <button className='btn btn--primary'>Quay lại trang chủ</button>
+      </div>
+    </div>
+  );
 };
