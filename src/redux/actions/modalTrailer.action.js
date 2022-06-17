a5bd7281f085ed6005fe42f1fc1e30be0934e261
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-17 21:31:03 +0700

    Chore: remove folder redux

--- src/redux/actions/modalTrailer.action.js
+++
@@ -1,9 +0,0 @@
-import { OPEN_TRAILER, CLOSE_TRAILER } from "redux/constants/modalTrailer.constant";
-
-export const openModalTrailer = (urlTrailer) => {
-  return { type: OPEN_TRAILER, payload: urlTrailer };
-};
-
-export const closeModalTrailer = () => {
-  return { type: CLOSE_TRAILER };
-};
--- src/redux/constants/modalTrailer.constant.js
+++
@@ -1,2 +0,0 @@
-export const OPEN_TRAILER = "OPEN_TRAILER";
-export const CLOSE_TRAILER = "CLOSE_TRAILER";
