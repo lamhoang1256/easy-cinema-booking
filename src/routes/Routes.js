42725d6886c80a9dcd74155573a16457654e9a7a
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-23 15:52:00 +0700

    Chore: format Routes file

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,37 +2,36 @@ import { path } from "constants/path";
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-const SignUp = React.lazy(() => import("pages/Authentication/SignUp"));
-const SignIn = React.lazy(() => import("pages/Authentication/SignIn"));
-const MovieDetail = React.lazy(() => import("pages/MovieDetail/MovieDetail"));
-const Booking = React.lazy(() => import("pages/Booking/Booking"));
-// need optimize
-const DashboardLayout = React.lazy(() => import("module/dashboard/DashboardLayout"));
-const CinemaView = React.lazy(() => import("module/cinema/CinemaView"));
 const MainLayout = React.lazy(() => import("layouts/MainLayout"));
+const Dashboard = React.lazy(() => import("pages/Dashboard/Dashboard"));
+const DashboardLayout = React.lazy(() => import("module/dashboard/DashboardLayout"));
 const Home = React.lazy(() => import("pages/Home/Home"));
-const UserProfile = React.lazy(() => import("module/user/UserProfile"));
 const Article = React.lazy(() => import("pages/Article/Article"));
-const NotFound = React.lazy(() => import("pages/NotFound/NotFound"));
 const UserManage = React.lazy(() => import("module/user/UserManage"));
+const UserProfile = React.lazy(() => import("module/user/UserProfile"));
 const UserUpdate = React.lazy(() => import("module/user/UserUpdate"));
 const UserHistory = React.lazy(() => import("module/user/UserHistory"));
+const Booking = React.lazy(() => import("pages/Booking/Booking"));
 const BookingHistory = React.lazy(() => import("module/booking/BookingHistory"));
 const MovieManage = React.lazy(() => import("module/movie/MovieManage"));
-const ComplexesManage = React.lazy(() => import("module/cinema/ComplexesManage"));
-const Dashboard = React.lazy(() => import("pages/Dashboard/Dashboard"));
+const MovieDetail = React.lazy(() => import("pages/MovieDetail/MovieDetail"));
 const MovieAddNew = React.lazy(() => import("module/movie/MovieAddNew"));
+const MovieUpdate = React.lazy(() => import("module/movie/MovieUpdate"));
+const MovieView = React.lazy(() => import("module/movie/MovieView"));
 const ShowtimeManage = React.lazy(() => import("module/showtime/ShowtimeManage"));
 const ShowtimeAddNew = React.lazy(() => import("module/showtime/ShowtimeAddNew"));
 const ShowtimeUpdate = React.lazy(() => import("module/showtime/ShowtimeUpdate"));
 const ShowtimeView = React.lazy(() => import("module/showtime/ShowtimeView"));
-const MovieUpdate = React.lazy(() => import("module/movie/MovieUpdate"));
-const MovieView = React.lazy(() => import("module/movie/MovieView"));
+const ComplexesManage = React.lazy(() => import("module/cinema/ComplexesManage"));
 const CinemaManage = React.lazy(() => import("module/cinema/CinemaManage"));
+const CinemaView = React.lazy(() => import("module/cinema/CinemaView"));
+const SignUp = React.lazy(() => import("pages/Authentication/SignUp"));
+const SignIn = React.lazy(() => import("pages/Authentication/SignIn"));
+const NotFound = React.lazy(() => import("pages/NotFound/NotFound"));
 
 const RoutesComponent = () => {
   return (
-    <Suspense fallback={<>Error</>}>
+    <Suspense fallback={<></>}>
       <Router>
         <Routes>
           <Route element={<MainLayout />}>
