f777579070eb47e27f5e21e821a7bf40e0223ee2
lamhoang1256 <lamhoang1256@gmail.com>
2022-06-26 16:10:11 +0700

    Refactor: optimize folder assets/styles

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "antd/dist/antd.css";
+import "assets/styles/reset.scss";
 import "assets/styles/main.scss";
 
 const App = () => {
